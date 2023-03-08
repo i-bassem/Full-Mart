71c8e1cb50a26e06790ca06481a66ef6c68e25fc
Bassem <RRR@example,com>
2023-03-08 22:42:20 +0200

    Initial try

---
+++ FullMart.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using FullMart.Core.UnitOfWork;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FullMart.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_unitOfWork.Categories.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public  IActionResult GetById(int id)
+        {
+            return Ok( _unitOfWork.Categories.GetById(c => c.Id == id));
+                 
+        }
+
+
+    }
+}
