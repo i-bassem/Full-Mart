e444265e288228552fad01ea400ea5194390c6b8
ahmed mohsen <ah.mohsen75@gmail.com>
2023-03-09 11:45:37 +0200

    Adding Delete End point

---
+++ FullMart.Data/Repositories/WishListProductRepo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FullMart.Core.Interfaces;
+using FullMart.Core.Models;
+using FullMart.Data.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullMart.Data.Repositories
+{
+    public class WishListProductRepo : BaseRepositiory<WishListProduct>, IWishListProductRepo
+    {
+        private readonly ApplicationDbContext _context;
+        public WishListProductRepo(ApplicationDbContext context) :base(context)
+        {
+            _context = context;
+        }
+
+        public void AddProductToUserWishList(string userId, int ProductId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProductFromUserCart(string userId, int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<WishList>> GetProductByUserIdAsync(string UserName)
+        {
+            return await _context.Set<WishList>()
+                .Include(a => a.AppUser)
+                .Include(wp => wp.WishListProducts)
+                .ThenInclude(wp => wp.Product)
+                .Where(p => p.AppUser.UserName == UserName)
+                .ToListAsync();
+        }
+    }
+}
