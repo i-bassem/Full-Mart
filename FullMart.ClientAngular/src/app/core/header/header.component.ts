19e9a82e0cf5e13c1a9ad656fe48dbe3a4146fed
Bassem <RRR@example,com>
2023-03-24 22:07:06 +0200

    User Profile

--- FullMart.ClientAngular/src/app/core/header/header.component.ts
+++ FullMart.ClientAngular/src/app/core/header/header.component.ts
@@ -34,10 +34,9 @@ export class HeaderComponent {
 
    //Search
    this.productService.getAllProducts().subscribe(product=>
-      
     this.productList=product
-
    )
+
 
 
   }
--- FullMart.ClientAngular/src/app/home/home.component.ts
+++ FullMart.ClientAngular/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
 import { CategoryListComponent } from '../Full-mart-modules/Category/category-list/category-list.component';
+import { IProduct } from '../_models/iproduct';
+import { ProductsService } from '../_services/Products/products.service';
 
 
 @Component({
@@ -8,5 +11,11 @@ import { CategoryListComponent } from '../Full-mart-modules/Category/category-li
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  
+
+
+
+
+
 
 }
