e07557207e075d3690fdea393fb284c28f61b73d
mohamed elshazly <113934540+Shazllyy@users.noreply.github.com>
2023-03-25 03:59:07 +0200

    ahow the last order and basic home page with sliders

--- FullMart.ClientAngular/src/app/Full-mart-modules/ordes/orders-list/orders-list.component.ts
+++ FullMart.ClientAngular/src/app/Full-mart-modules/ordes/orders-list/orders-list.component.ts
@@ -1,4 +1,50 @@
 
+// import { Component, OnInit } from '@angular/core';
+// import { OrderService } from 'src/app/_services/order/order.service';
+
+// import { Iorder } from 'src/app/_models/Iorder';
+
+// @Component({
+//   selector: 'app-orders-list',
+//   templateUrl: './orders-list.component.html',
+//   styleUrls: ['./orders-list.component.css']
+// })
+
+// export class OrdersListComponent implements OnInit {
+
+//   userId = localStorage.getItem('id');
+//   orders: Iorder[] | undefined;
+
+//   // orders: Order[] = [...]; // Your list of orders
+
+  
+//   constructor(private orderService: OrderService) { }
+//   calculateTotalPrice(order: any): number {
+//     let totalPrice = 0;
+//     for (let orderProduct of order.orderProductDTOs) {
+//       totalPrice += orderProduct.price;
+//     }
+//     return totalPrice;
+//   }
+//   getAllOrders(): void {
+//     if(this.userId!=null)
+
+//     this.orderService.getOrdersByUserId(this.userId)
+//       .subscribe(orders => this.orders = orders);
+//   }
+
+  
+//   ngOnInit(): void {
+//     if(this.userId!=null)
+//     {console.log(this.userId)
+//     this.orderService.getOrdersByUserId(this.userId)
+//       .subscribe(orders => this.orders = orders);}
+//   }
+
+  
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from 'src/app/_services/order/order.service';
 
@@ -15,13 +61,35 @@ export class OrdersListComponent implements OnInit {
   userId = localStorage.getItem('id');
   orders: Iorder[] | undefined;
 
-  constructor(private orderService: OrderService) { }
+  // orders: Order[] = [...]; // Your list of orders
 
-  ngOnInit(): void {
-    if(this.userId!=null)
-    {console.log(this.userId)
-    this.orderService.getOrdersByUserId(this.userId)
-      .subscribe(orders => this.orders = orders);}
+  
+  constructor(private orderService: OrderService) { }
+  calculateTotalPrice(order: any): number {
+    let totalPrice = 0;
+    for (let orderProduct of order.orderProductDTOs) {
+      totalPrice += orderProduct.price;
+    }
+    return totalPrice;
   }
+  getAllOrders(): void {
+       if(this.userId!=null)
+    
+        this.orderService.getOrdersByUserId(this.userId)
+         .subscribe(orders => this.orders = orders);
+     }
+
+  
+  ngOnInit(): void {
+    if (this.userId != null) {
+      console.log(this.userId)
+      this.orderService.getOrdersByUserId(this.userId)
+        .subscribe(orders => {
+          this.orders = orders;
+          this.orders = [this.orders[this.orders.length - 1]]; // Get the last order
+        });
+    } }
+
+  
 }
 
--- FullMart.ClientAngular/src/app/home/home.component.ts
+++ FullMart.ClientAngular/src/app/home/home.component.ts
@@ -1,21 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { IBrandDTO } from 'src/app/_models/Ibranddto';
+import { ICategory } from 'src/app/_models/ICategory';
+import { IProduct } from 'src/app/_models/iproduct';
+import { BrandService } from 'src/app/_services/Brand/Brands.service';
+import { CartService } from 'src/app/_services/Cart/cart.service';
+import { CategoriesService } from 'src/app/_services/Categories/categories.service';
+import { ProductsService } from 'src/app/_services/Products/products.service';
 import { environment } from 'src/environments/environment.development';
-import { CategoryListComponent } from '../Full-mart-modules/Category/category-list/category-list.component';
-import { IProduct } from '../_models/iproduct';
-import { ProductsService } from '../_services/Products/products.service';
-
+import { WishlistProductService } from 'src/app/_services/Wishlist/wishlist-product.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(
+    private catservice: CategoriesService,
+    public router: Router,
+    private productServices: ProductsService
+  ) {}
+
+  catList: ICategory[] = [];
+  category: ICategory | null = null;
+  products: IProduct[] = [];
+  serverURL = `${environment.ImgURL}`;
+
+  ngOnInit(): void {
+    this.catservice.getAllCategories().subscribe((cat) => (this.catList = cat));
+    this.productServices.getAllProducts().subscribe((res: IProduct[]) => {
+      this.products = res;
+    });
+    
+  }
+
+  ngOnChanges(): void {
+    this.catservice.getAllCategories().subscribe((cat) => (this.catList = cat));
+  }
+
+  createImagepath(serverPath: string) {
+    return `${environment.ImgURL + serverPath}`;
+  }
+  getProductsByCategory(categoryId: string): IProduct[] {
+    return this.products.filter((product) => product.categoryName === categoryId);
+  }
   
-
-
-
-
-
-
+  delete(catID: number) {
+    this.catservice.deleteCategory(catID).subscribe((data) => console.log(data));
+    // setTimeout(()=>this.router.navigateByUrl("/category"),1500)
+    window.location.reload();
+  }
 }
+
